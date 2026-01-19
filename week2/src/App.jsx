500dc8273e3c0c761fd5c25ecbe7afe80403646d
Angel Uriel Del Villar calderón <za220120331@zapopan.tecmm.edu.mx>
2026-01-19 15:28:06 -0600

    muiButton: Add MUI Button component for Week 2 challenge

---
+++ week2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MuiButtonExample from "./components/muiButton";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Week 2 Challenge</h1>
+      <MuiButtonExample />
+    </div>
+  );
+}
